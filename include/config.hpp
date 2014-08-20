158eb35668318b35185c867692799ce0a5437c65
Johannes Jendersie <jojendersie@web.de>
2014-08-20 11:19:27 +0200

    option for utf8 namespace

---
+++ include/config.hpp
@@ -0,0 +1,12 @@
+﻿/// \brief Include or uncomment lines to customize namespaces and similar.
+///
+
+
+/// \brief Using this option changes the namespace gam:: to γ::.
+/// \details Do not use this if the symbol gamma above is not shown correctly,
+///    or enable utf8 in all your files.
+///
+///    The default is off.
+//#define USE_UTF8_NAMESPACES
+
+#include "config.inl"
