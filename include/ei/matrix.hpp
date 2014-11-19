7dd8300b4fefc49393b9d25f4cdf4964f4a9a287
Johannes Jendersie <jojendersie@web.de>
2014-11-19 10:18:53 +0100

    using Cpp11 type aliasing to define general vector types

--- include/ei/matrix.hpp
+++ include/ei/matrix.hpp
@@ -284,20 +284,24 @@ namespace ei {
 
     // ********************************************************************* //
     // Predefined float vector and matrix types.
+    /// \brief General column-vector type
+    template<typename T, unsigned N> using Vec  = Matrix<T, N, 1>;
+    /// \brief General row-vector type
+    template<typename T, unsigned N> using RVec = Matrix<T, 1, N>;
 
     /// \brief 2D column-vector of type float.
-    typedef Matrix<float, 2, 1> Vec2;
+    typedef Vec<float, 2> Vec2;
     /// \brief 3D column-vector of type float.
-    typedef Matrix<float, 3, 1> Vec3;
+    typedef Vec<float, 3> Vec3;
     /// \brief 4D column-vector of type float.
-    typedef Matrix<float, 4, 1> Vec4;
+    typedef Vec<float, 4> Vec4;
 
     /// \brief 2D row-vector of type float.
-    typedef Matrix<float, 1, 2> RVec2;
+    typedef RVec<float, 2> RVec2;
     /// \brief 3D row-vector of type float.
-    typedef Matrix<float, 1, 3> RVec3;
+    typedef RVec<float, 3> RVec3;
     /// \brief 4D row-vector of type float.
-    typedef Matrix<float, 1, 4> RVec4;
+    typedef RVec<float, 4> RVec4;
 
     /// \brief 2x2 matrix of type float.
     typedef Matrix<float, 2, 2> Mat2x2;
@@ -310,18 +314,18 @@ namespace ei {
     // Predefined 32 bit integer vector and matrix types.
 
     /// \brief 2D column-vector of type int32.
-    typedef Matrix<int32, 2, 1> IVec2;
+    typedef Vec<int32, 2> IVec2;
     /// \brief 3D column-vector of type int32.
-    typedef Matrix<int32, 3, 1> IVec3;
+    typedef Vec<int32, 3> IVec3;
     /// \brief 4D column-vector of type int32.
-    typedef Matrix<int32, 4, 1> IVec4;
+    typedef Vec<int32, 4> IVec4;
 
     /// \brief 2D row-vector of type int32.
-    typedef Matrix<int32, 1, 2> IRVec2;
+    typedef RVec<int32, 2> IRVec2;
     /// \brief 3D row-vector of type int32.
-    typedef Matrix<int32, 1, 3> IRVec3;
+    typedef RVec<int32, 3> IRVec3;
     /// \brief 4D row-vector of type int32.
-    typedef Matrix<int32, 1, 4> IRVec4;
+    typedef RVec<int32, 4> IRVec4;
 
     /// \brief 2x2 matrix of type int32.
     typedef Matrix<int32, 2, 2> IMat2x2;
@@ -334,18 +338,18 @@ namespace ei {
     // Predefined 32 bit unsigned integer vector and matrix types.
 
     /// \brief 2D column-vector of type uint32.
-    typedef Matrix<uint32, 2, 1> UVec2;
+    typedef Vec<uint32, 2> UVec2;
     /// \brief 3D column-vector of type uint32.
-    typedef Matrix<uint32, 3, 1> UVec3;
+    typedef Vec<uint32, 3> UVec3;
     /// \brief 4D column-vector of type uint32.
-    typedef Matrix<uint32, 4, 1> UVec4;
+    typedef Vec<uint32, 4> UVec4;
 
     /// \brief 2D row-vector of type uint32.
-    typedef Matrix<uint32, 1, 2> URVec2;
+    typedef RVec<uint32, 2> URVec2;
     /// \brief 3D row-vector of type uint32.
-    typedef Matrix<uint32, 1, 3> URVec3;
+    typedef RVec<uint32, 3> URVec3;
     /// \brief 4D row-vector of type uint32.
-    typedef Matrix<uint32, 1, 4> URVec4;
+    typedef RVec<uint32, 4> URVec4;
 
     /// \brief 2x2 matrix of type uint32.
     typedef Matrix<uint32, 2, 2> UMat2x2;
