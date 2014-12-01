20962454480c16211234972de6bad0e215f60c2c
Johannes Jendersie <jojendersie@web.de>
2014-12-01 16:14:03 +0100

    resorted header for better overview of intersection variants

--- include/ei/3dintersection.hpp
+++ include/ei/3dintersection.hpp
@@ -11,59 +11,45 @@ namespace ei {
     inline bool intersects( const Ellipsoid& _ellipsoid, const Vec3& _point )  { return intersects( _point, _ellipsoid ); }
 
     /// \brief Do a ray and an ellipsoid intersect or touch?
-    /// \details Performance index: 23.1
-    /// \return true if there is at least one comon point between ray and ellipsoid
-    bool intersects( const Ray& _ray, const Ellipsoid& _ellipsoid );           // TESTED
-    inline bool intersects( const Ellipsoid& _ellipsoid, const Ray& _ray )  { return intersects( _ray, _ellipsoid ); }
-
-    /// \brief Get the distance of the first intersection if hit.
-    /// \details Performance index: 23.9
-    /// \param [out] _distance The ray parameter (distance) for the first
+    /// \details Performance index: 23.1, 23.9 (Numbers for increasing
+    ///     usage of optional parameters)
+    /// \param [out,opt] _distance The ray parameter (distance) for the first
     ///     intersection point in positive direction.
     ///
     ///     If the ray starts inside 0 is returned.
-    /// \return true if there is at least one comon point between ray and ellipsoid
+    /// \return true if there is at least one comon point between ray and
+    ///     ellipsoid.
+    bool intersects( const Ray& _ray, const Ellipsoid& _ellipsoid );           // TESTED
     bool intersects( const Ray& _ray, const Ellipsoid& _ellipsoid, float& _distance );
+    inline bool intersects( const Ellipsoid& _ellipsoid, const Ray& _ray )  { return intersects( _ray, _ellipsoid ); }
     inline bool intersects( const Ellipsoid& _ellipsoid, const Ray& _ray, float& _distance )  { return intersects( _ray, _ellipsoid, _distance ); }
 
     /// \brief Do a ray and a box intersect or touch?
-    /// \details Performance index: 17.1
-    /// \return true if there is at least one comon point between ray and box
-    bool intersects( const Ray& _ray, const Box& _box );                       // TESTED
-    inline bool intersects( const Box& _box, const Ray& _ray )  { return intersects( _ray, _box ); }
-
-    /// \brief Get the distance of the first intersection if hit.
-    /// \details Performance index: 18.0
-    /// \param [out] _distance The ray parameter (distance) for the first
+    /// \details Performance index: 17.1, 18.0 (Numbers for increasing
+    ///     usage of optional parameters)
+    /// \param [out,opt] _distance The ray parameter (distance) for the first
     ///     intersection point in positive direction.
     ///
     ///     If the ray starts inside 0 is returned.
     /// \return true if there is at least one comon point between ray and box
+    bool intersects( const Ray& _ray, const Box& _box );                       // TESTED
     bool intersects( const Ray& _ray, const Box& _box, float& _distance );
+    inline bool intersects( const Box& _box, const Ray& _ray )  { return intersects( _ray, _box ); }
     inline bool intersects( const Box& _box, const Ray& _ray, float& _distance )  { return intersects( _ray, _box, _distance ); }
 
     /// \brief Do a ray and a triangle intersect or touch?
-    /// \details Performance index: 12.3
-    /// \return true if there is at least one comon point between ray and triangle
-    bool intersects( const Ray& _ray, const Triangle& _triangle );             // TESTED
-    inline bool intersects( const Triangle& _triangle, const Ray& _ray )  { return intersects( _ray, _triangle ); }
-
-    /// \brief Get the distance of the first intersection if hit.
-    /// \details Performance index: 13.7
-    /// \param [out] _distance The ray parameter (distance) for the first
+    /// \details Performance index: 12.3, 13.7, TODO (Numbers for increasing
+    ///     usage of optional parameters)
+    /// \param [out,opt] _distance The ray parameter (distance) for the first
     ///     intersection point in positive direction.
-    /// \return true if there is at least one comon point between ray and triangle
-    bool intersects( const Ray& _ray, const Triangle& _triangle, float& _distance );    // TESTED
-    inline bool intersects( const Triangle& _triangle, const Ray& _ray, float& _distance )  { return intersects( _ray, _triangle, _distance ); }
-
-    /// \brief Get the distance of the first intersection if hit and the
-    ///     barycentric coordinates.
-    /// \details Performance index: TODO
-    /// \param [out] _distance The ray parameter (distance) for the first
-    ///     intersection point in positive direction.
-    /// \param [out] _barycentric The barycentric coordinates of the hit point
+    /// \param [out,opt] _barycentric The barycentric coordinates of the hit point
     ///     on the triangle.
-    /// \return true if there is at least one comon point between ray and triangle
+    /// \return true if there is at least one comon point between ray and triangle.
+    ///     This point has a ray parameter >= 0 (no negative direction).
+    bool intersects( const Ray& _ray, const Triangle& _triangle );                                          // TESTED
+    bool intersects( const Ray& _ray, const Triangle& _triangle, float& _distance );                        // TESTED
     bool intersects( const Ray& _ray, const Triangle& _triangle, float& _distance, Vec3& _barycentric );    // TESTED
+    inline bool intersects( const Triangle& _triangle, const Ray& _ray )  { return intersects( _ray, _triangle ); }
+    inline bool intersects( const Triangle& _triangle, const Ray& _ray, float& _distance )  { return intersects( _ray, _triangle, _distance ); }
     inline bool intersects( const Triangle& _triangle, const Ray& _ray, float& _distance, Vec3& _barycentric )  { return intersects( _ray, _triangle, _distance, _barycentric ); }
 }
