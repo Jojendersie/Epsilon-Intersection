892c192eb9bc5fb46773a1180d31387f79223ad0
Johannes Jendersie <jojendersie@web.de>
2015-09-25 09:31:59 +0200

    added overflow handling to the Fix32 conversion methods

--- include/ei/fixedpoint.hpp
+++ include/ei/fixedpoint.hpp
@@ -2,25 +2,57 @@
 
 #include "elementarytypes.hpp"
 
+namespace details {
+    // Helper shift for signed right operands. It corresponds to a right shift
+    // for positive values and to a left shift for negative ones.
+    ei::uint32 xshift(ei::uint32 _value, int _count)
+    {
+        return (_count < 0) ? _value << -_count : _value >> _count;
+    }
+
+    bool overflows(ei::uint32 _value, int _leftShift)
+    {
+        /// Create a bit mask with 111..0000 wher the number of ones is _leftShift.
+        return (_value & (0xffffffffu << (32-_leftShift))) != 0;
+    }
+}
+
 namespace ei
 {
-    /// Signed fixed point numbers. You can freely chose a size
+    /// Signed fixed point numbers. You can freely choose a size
     /// (8, 16 or 32*n) and the number of binary digits after the binary point.
+    ///
+    /// The internal representation is based on two complements and inherites
+    /// some properties. The negative maximum is always one step large in its
+    /// absolute value than its positive: |nmax| > max.
+    /// E.g.: Fix<32,31> has the minimum -1 and the maximum (2^31-1)/(2^31)
+    /// (that is 0.9999999995343387126922607421875).
+    /// This also includes that abs(nmax) = nmax, because it immediatelly
+    /// overflows on conversion (note that this is the same for common integer
+    /// types. E.g. for int32: abs(-2147483648) == -2147483648).
+    ///
+    /// Fixed point numbers can be converted from and two float/double. Thereby
+    /// too large and too small values are clamped to the positive and negative
+    /// maxima.
+    /// NaNs are converted to nmax.
     template<unsigned BitSize, unsigned FractionalBits>
     struct Fix
     {
         enum {NUM_INTS = BitSize/32};
         static_assert(FractionalBits < BitSize, "Number of fractional bits larger than entire type! Remember: there is a sign bit.");
         static_assert(NUM_INTS*32 == BitSize, "BitSize is not a multiple of 32!");
+        static_assert(NUM_INTS >= 2, "There is a 32 bit specialization. This class should only be compiled for larger types!");
 
         enum {
-            NUM_INT_DIGITS2  = BitSize - FractionalBits,
+            NUM_INT_DIGITS2  = BitSize - FractionalBits - 1,
             NUM_INT_DIGITS10 = int((BitSize - FractionalBits) / 3.321928095 + 1),
             NUM_FRAC_DIGITS2 = FractionalBits,
             NUM_FRAC_DIGITS10 = FractionalBits,
         };
 
-        int32 intrep[NUM_INTS];      ///< Internal integer representation
+        /// TODO: there is no? guarantee what happens with negative numbers on shift
+        /// maybe it is necessary to store all but the first as uint32.
+        int32 intrep[NUM_INTS];      ///< Internal integer representation with [0] as the most significant word
     private:
         typedef Fix<BitSize, FractionalBits> T;
     };
@@ -69,19 +101,32 @@ namespace ei
     template<unsigned FractionalBits>
     Fix<32, FractionalBits>::Fix(float _value)
     {
-        intrep = int32(_value * double(1u << FractionalBits));
+        // Convert to a large int by shifting (i.e. change exponent).
+        double val = _value * double(1u << FractionalBits);
+        // Check for under and overflow
+        if(val >= double(1u << 31)) intrep = 0x7fffffff;
+        else if(val <= -double(1u << 31)) intrep = 0x80000000;
+        else intrep = int32(val);
     }
 
     template<unsigned FractionalBits>
     Fix<32, FractionalBits>::Fix(double _value)
     {
+        // Convert to a large int by shifting (i.e. change exponent).
+        double val = _value * double(1u << FractionalBits);
+        // Check for under and overflow
+        if(val >= double(1u << 31)) intrep = 0x7fffffff;
+        else if(val <= -double(1u << 31)) intrep = 0x80000000;
+        else intrep = int32(val);
         intrep = int32(_value * double(1u << FractionalBits));
     }
 
     template<unsigned FractionalBits>
     Fix<32, FractionalBits>::Fix(int _value)
     {
-        intrep = int32(_value << FractionalBits);
+        if(details::overflows(_value & 0x7fffffff, FractionalBits+1))
+            intrep = _value < 0 ? 0x80000000 : 0x7fffffff;
+        else intrep = int32(_value << FractionalBits);
     }
 
     // ********************************************************************* //
--- unittest/fixedpoint.cpp
+++ unittest/fixedpoint.cpp
@@ -10,54 +10,79 @@ bool test_fixedpoint()
 {
     bool result = true;
 
-    // Casting
-    Fix<32,16> f0(0.0f);
-    Fix<32,16> f1(-0.0);
-    Fix<32,16> f2 = static_cast<Fix<32,16>>(1);
-    Fix<32,16> f3 = static_cast<Fix<32,16>>(0.1);
-    Fix<32,31> f4(-0.25f);
-    Fix<32,31> f5(0.1f);
-    TEST( float(f0) == 0.0f, "Converting 0 to Fix<32,16> and back to float failed!" );
-    TEST( double(f1) == 0.0f, "Converting 0 to Fix<32,16> and back to double failed!" );
-    TEST( float(f2) == 1.0f, "Converting 1 to Fix<32,16> and back to float failed!" );
-    TEST( approx(float(f3), 0.1f, 1e-5f), "Converting 0.1 to Fix<32,16> and back to float failed!" );
-    TEST( float(f4) == -0.25f, "Converting -0.25f to Fix<32,31> and back to float failed!" );
-    TEST( float(f5) == 0.1f, "Converting 0.1 to Fix<32,31> and back to float failed!" );
+    {
+        // Casting
+        Fix<32,16> f0(0.0f);
+        Fix<32,16> f1(-0.0);
+        Fix<32,16> f2 = static_cast<Fix<32,16>>(1);
+        Fix<32,16> f3 = static_cast<Fix<32,16>>(0.1);
+        Fix<32,31> f4(-0.25f);
+        Fix<32,31> f5(0.1f);
+        TEST( float(f0) == 0.0f, "Converting 0 to Fix<32,16> and back to float failed!" );
+        TEST( double(f1) == 0.0f, "Converting 0 to Fix<32,16> and back to double failed!" );
+        TEST( float(f2) == 1.0f, "Converting 1 to Fix<32,16> and back to float failed!" );
+        TEST( approx(float(f3), 0.1f, 1e-5f), "Converting 0.1 to Fix<32,16> and back to float failed!" );
+        TEST( float(f4) == -0.25f, "Converting -0.25f to Fix<32,31> and back to float failed!" );
+        TEST( float(f5) == 0.1f, "Converting 0.1 to Fix<32,31> and back to float failed!" );
 
-    // Fixed point unary minus
-    TEST( float(-Fix<32,31>(-0.2)) == 0.2f, "Unary minus of Fix<32,31> failed!" );
+        // Fixed point unary minus and overflow
+        Fix<32,31> fmax0(2.0f);
+        Fix<32,31> fmin0(-2.0f);
+        double d = double(fmax0);
+        Fix<32,31> fmax1(d);
+        d = double(fmin0);
+        Fix<32,31> fmin1(d);
+        Fix<32,31> fnan(NAN);
+        Fix<32,31> fmax2(1);
+        Fix<32,31> fmin2(-1);
+        Fix<32,31> fmin3(-3);
+        TEST( float(-Fix<32,31>(-0.2)) == 0.2f, "Unary minus of Fix<32,31> failed!" );
+        TEST( fmax0.intrep == 0x7fffffff, "Overflow did not yield fix-max!" );
+        TEST( fmin0.intrep == 0x80000000, "Overflow (negative) did not yield fix-min!" );
+        TEST( fmax1.intrep == 0x7fffffff, "Overflowed number not correct!" );
+        TEST( fmin1.intrep == 0x80000000, "Overflowed (negative) number not correct!" );
+        TEST( fnan.intrep == 0x80000000, "NaNs are expected to be the maximum negative representable number!" );
+        TEST( fmax2.intrep == 0x7fffffff, "From Int: Overflow did not yield fix-max!" );
+        TEST( fmin2.intrep == 0x80000000, "From Int: -1 not correctly converted!" );
+        TEST( fmin3.intrep == 0x80000000, "From Int: Overflow (negative) did not yield fix-min!" );
 
-    // Addition and subtraction
-    Fix<32,16> f6 = static_cast<Fix<32,16>>(0.5f);
-    Fix<32,16> f7 = static_cast<Fix<32,16>>(2.0f);
-    TEST( (Fix<32,16>(1.1f)) == f2 + f3, "Addition f2 + f3 failed!" );
-    TEST( f6 == f2 + -f6, "Addition f2 + -f6 failed!" );
-    TEST( f6 == f2 - f6, "Subtraction f2 - f6 failed!" );
-    TEST( f3 == f2 * f3, "Multiplication f2 * f3 failed!" );
-    TEST( (Fix<32,16>(0.05f)) == f3 * f6, "Multiplication f3 * f6 failed!" );
-    TEST( f7 == f2 / f6, "Division f2 / f6 failed!" );
+        // Addition and subtraction
+        Fix<32,16> f6 = static_cast<Fix<32,16>>(0.5f);
+        Fix<32,16> f7 = static_cast<Fix<32,16>>(2.0f);
+        TEST( (Fix<32,16>(1.1f)) == f2 + f3, "Addition f2 + f3 failed!" );
+        TEST( f6 == f2 + -f6, "Addition f2 + -f6 failed!" );
+        TEST( f6 == f2 - f6, "Subtraction f2 - f6 failed!" );
+        TEST( f3 == f2 * f3, "Multiplication f2 * f3 failed!" );
+        TEST( (Fix<32,16>(0.05f)) == f3 * f6, "Multiplication f3 * f6 failed!" );
+        TEST( f7 == f2 / f6, "Division f2 / f6 failed!" );
 
-    // Comparison
-    Fix<32,16> f8 = static_cast<Fix<32,16>>(-8.0f);
-    Fix<32,31> f9(0.5f);
-    TEST( f0 == f1, "0 and -0 should be equal!" );
-    TEST( f0 != f2, "f0 and f2 should be unequal!" );
-    TEST( f3 <= f3, "f3 and f3 are equal!" );
-    TEST( !(f3 < f3), "f3 and f3 are equal!" );
-    TEST( f0 < f3, "f0 is smaller than f3!" );
-    TEST( !(f0 > f3), "f0 is not larger than f3!" );
-    TEST( f0 > f8, "f0 is greater than f8!" );
-    TEST( f5 >= f4, "f5 is greater than f4!" );
-    TEST( f4 < f5, "f4 is smaller than f5!" );
-    TEST( f5 < f9, "f4 is smaller than f9!" );
+        // Comparison
+        Fix<32,16> f8 = static_cast<Fix<32,16>>(-8.0f);
+        Fix<32,31> f9(0.5f);
+        TEST( f0 == f1, "0 and -0 should be equal!" );
+        TEST( f0 != f2, "f0 and f2 should be unequal!" );
+        TEST( f3 <= f3, "f3 and f3 are equal!" );
+        TEST( !(f3 < f3), "f3 and f3 are equal!" );
+        TEST( f0 < f3, "f0 is smaller than f3!" );
+        TEST( !(f0 > f3), "f0 is not larger than f3!" );
+        TEST( f0 > f8, "f0 is greater than f8!" );
+        TEST( f5 >= f4, "f5 is greater than f4!" );
+        TEST( f4 < f5, "f4 is smaller than f5!" );
+        TEST( f5 < f9, "f4 is smaller than f9!" );
 
-    int i0 = Fix<32,16>::NUM_INT_DIGITS2;
-    int i1 = Fix<32,16>::NUM_INT_DIGITS10;
-    int i2 = Fix<32,16>::NUM_FRAC_DIGITS2;
-    int i3 = Fix<32,16>::NUM_FRAC_DIGITS10;
+        int i0 = Fix<32,16>::NUM_INT_DIGITS2;
+        int i1 = Fix<32,16>::NUM_INT_DIGITS10;
+        int i2 = Fix<32,16>::NUM_FRAC_DIGITS2;
+        int i3 = Fix<32,16>::NUM_FRAC_DIGITS10;
 
-//    char buf[128];
-//    f4.toString(buf);
+    //    char buf[128];
+    //    f4.toString(buf);
+    }
+
+    // 64 BIT
+    {
+
+    }
 
     return result;
 }
